f406551e74c167c740730cc1daffba1518804168
anton5soft <anton@anton-AO521.(none)>
2014-06-02 18:23:01 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,9 @@ require.config({
 		angular: '../../bower_components/angular/angular',
 		angularRoute: '../../bower_components/angular-route/angular-route',
 		angularMocks: '../../bower_components/angular-mocks/angular-mocks',
-		text: '../../bower_components/requirejs-text/text'
+		text: '../../bower_components/requirejs-text/text',
+        jquery: ['//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min','lib/jquery/jquery-1.8.2.min'],
+        bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap.min'
 	},
 	shim: {
 		'angular' : {'exports' : 'angular'},
@@ -11,7 +13,11 @@ require.config({
 		'angularMocks': {
 			deps:['angular'],
 			'exports':'angular.mock'
-		}
+		},
+        'jquery' : {'exports' : 'jquery'},
+        'bootstrap':{
+            deps: ['jquery']
+        }
 	},
 	priority: [
 		"angular"
@@ -24,8 +30,9 @@ window.name = "NG_DEFER_BOOTSTRAP!";
 require( [
 	'angular',
 	'app',
-	'routes'
-], function(angular, app, routes) {
+	'routes',
+    'bootstrap'
+], function(angular, app, routes,bootstrap) {
 	'use strict';
 	var $html = angular.element(document.getElementsByTagName('html')[0]);
 
